7db0b34e1c2b81f112f1fe0a0530d22ddaa28be0
Dennis Padiernos <depadiernos@gmail.com>
2019-11-08 18:59:37 -0600

    add get request to fetch color data

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import api from "../utils/axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,9 +8,20 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    api()
+      .get("/colors")
+      .then(res => {
+        setColorList(res.data);
+      })
+      .catch(err => setError(err));
+  }, []);
 
   return (
     <>
+      {error && <div className="delete">{`${error}`}</div>}
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
